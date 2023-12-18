efe2977eca972319787a06a5577190a5d2f2efa2
HuynhNamKevin <153898691+HuynhNamKevin@users.noreply.github.com>
2023-12-18 18:58:53 +0700

    move left and right

---
+++ Assets/NamChoDien/Script/player/movement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float moveSpeed = 3;
+    public float leftRightSpeed = 4;
+    void Update()
+    {
+        //Move forward
+      transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
+        //Move left 
+      if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) 
+      {
+          if (transform.position.x > LevelBoundary.leftSide)
+          {
+              transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed);
+          }
+      }
+        //Move right
+      if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+      {
+          if (transform.position.x < LevelBoundary.rightSide)
+          {
+              transform.Translate(Vector3.right * Time.deltaTime * leftRightSpeed);
+          }
+      }
+    }
+}
